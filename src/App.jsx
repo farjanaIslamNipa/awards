e61dcd0e07e50da285ef6a306af6ae2cfa165812
farjanaIslamNipa <nipa.farzana87@gmail.com>
2025-05-20 13:36:46 +0600

    Configure theme font

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className=' font-bold font-circular-web'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, rerum.</div>
+      <div className=' font-bold font-robert-medium'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, rerum.</div>
+      <div className=' font-bold font-zentry'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, rerum.</div>
+    </div>
+  )
+}
+
+export default App
