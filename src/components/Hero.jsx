9d7da2c59201b0023bee1d2380a6543947e799d3
farjanaIslamNipa <nipa.farzana87@gmail.com>
2025-05-27 13:29:33 +0600

    complete adding animation to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,32 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button'
 import { TiLocationArrow } from 'react-icons/ti'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1)
   const [hasClicked, setHasClicked] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const [loadedVideos, setLoadedVideos] = useState(0)
 
-  const totalVideos = 3
+  const totalVideos = 4
   const nextVideoRef = useRef(null)
 
   const handleVideoLoad = () => {
-    setLoadedVideos(prev => prev + 1)
+    setLoadedVideos((prev) => prev + 1)
   }
 
-  const upcomingVideoIndex = (currentIndex % totalVideos) + 1
+  useEffect(() => {
+    if(loadedVideos === (totalVideos - 1)){
+      setLoading(false)
+    }
+  }, [loadedVideos])
 
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1
   const handleMiniVdClick = () => {
     setHasClicked(true)
     setCurrentIndex(upcomingVideoIndex)
@@ -48,9 +56,41 @@ const Hero = () => {
     }
   }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius: '0 0 40% 10%',
+    })
+
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true
+      }
+    })
+  })
+
+
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+      {
+        loading && (
+          <div className='flex-center absolute z-[100] h-dvh w-screen  overflow-x-hidden'>
+            <div className='three-body'>
+              <div className="three-body__dot" />
+              <div className="three-body__dot" />
+              <div className="three-body__dot" />
+            </div>
+          </div>
+        )
+      }
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
           <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
@@ -66,8 +106,16 @@ const Hero = () => {
               />
             </div>
           </div>
-
-          <video 
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video          
           src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)} 
           autoPlay
           loop
